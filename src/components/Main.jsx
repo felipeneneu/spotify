12c7421c927403ba2c712a939f67fe0b579c6730
felipeneneu <feliperpg3@gmail.com>
2025-02-13 00:16:29 -0300

    primeira aula do treinamento fullstack

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Main.css';
-import { FaCirclePlay } from 'react-icons/fa6';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+
 export const Main = () => {
   return (
     <main className='main'>
@@ -8,16 +10,36 @@ export const Main = () => {
           <h2>Artistas populares</h2>
           <a href="/" className='item-list__link'>Mostrar tudo</a>
         </div>
-        <div className='single-item'>
-          <div className="single-item__div-image-button">
-            <div className="single-item__div-image">
-              <img className='single-item__image' src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem Henrique & Juliano" />
+        <div className="item-list__container">
+          <div className='single-item'>
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img className='single-item__image' src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem Henrique & Juliano" />
+              </div>
+              <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
             </div>
-            <FaCirclePlay stroke='solid' color="green" fontSize="3rem" />
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className='single-item__title'>Henrique & Juliano</p>
+
+              </div>
+              <p className='single-item__type'>Artista</p>
+            </div>
           </div>
-          <div className="single-item__texts">
-            <p className='single-item__title'>Henrique & Juliano</p>
-            <p className='single-item__type'>Artista</p>
+
+          <div className='single-item'>
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img className='single-item__image' src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem Henrique & Juliano" />
+              </div>
+              <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+            </div>
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className='single-item__title'>Henrique & Juliano</p>
+                <p className='single-item__type'>Artista</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
